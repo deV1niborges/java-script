037375c63c4f5436a705b0b770c91b548df9f804
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2023-08-30 15:41:06 -0300

    aula 10 e 11 criadas

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidae do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida.MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
