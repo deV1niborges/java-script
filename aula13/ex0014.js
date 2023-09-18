b80a7dcf22187d756576cbb9ab74a2c80fe0eeb5
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2023-09-18 14:59:34 -0300

    aula 13 e 14 criada

---
+++ aula13/ex0014.js
@@ -0,0 +1,22 @@
+/* 
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+ } while (c <= 1) 
+*/
+
+/*
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+console.log('Tudo bem ?')
+*/
+
+var c = 1
+while (c <= 6) {
+    console.log('tudo bem?')
+    c++
+}
