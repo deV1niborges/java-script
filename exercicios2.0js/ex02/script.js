50d1070860084f3db2e4a272d7c72e8e7a709312
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2023-11-13 15:04:00 -0300

    update ex02

---
+++ exercicios2.0js/ex02/script.js
@@ -0,0 +1,15 @@
+/*
+
+Exemplo 5 = some os valores dados uma coleção de dados "idades" 
+[15,46,75,34,23],imprima na tela a soma deste valores.
+
+*/
+
+let idades = [15,46,75,34,23]
+let total = 0
+
+for (idades of idades) {
+    total = total + idades
+    console.log(total)
+}
+
