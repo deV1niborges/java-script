567d3fef777c682152b9fac4776fdcdd5fd81d5b
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2023-12-26 17:51:24 -0300

    aprimorando exercicios2.0js

--- exercicios2.0js/ex02/script.js
+++ exercicios2.0js/ex02/script.js
@@ -5,11 +5,23 @@ Exemplo 5 = some os valores dados uma coleção de dados "idades"
 
 */
 
-let idades = [15,46,75,34,23]
+let idades = [15,46,75,34,23]  // Metódo for
 let total = 0
 
-for (idades of idades) {
-    total = total + idades
-    console.log(total)
+for(var i = 0; i < idades.length; i++) {
+    total += idades[i]
 }
+
+console.log(total)
+
+let idades2 = [15,46,75,34,23]  // Metódo reduce
+var total2 =  idades2.reduce(function(accumulator,value){
+    return accumulator + value
+},0) 
+
+console.log(total2)
+     
+
+
+
 
