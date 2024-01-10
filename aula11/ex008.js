f1c24e400e3fee267b8efff64fd7397e37a1e4cf
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2024-01-10 11:56:03 -0300

    melhoria nos codigos

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 60
-console.log(`A velocidae do seu carro é ${vel}km/h`)
+var vel = 60;
+console.log(`A velocidae do seu carro é ${vel}km/h`);
 if (vel > 60) {
-    console.log(`Você ultrapassou a velocidade permitida.MULTADO!`)
+  console.log(`Você ultrapassou a velocidade permitida.MULTADO!`);
 }
-console.log(`Dirija sempre com cinto de segurança`)
+console.log(`Dirija sempre com cinto de segurança`);
--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,11 +1,11 @@
-var pais = 'br'
+var pais = "br";
 
-if (pais.trim() === '') {
-    console.log('Você não forneceu um país válido.');
-} else if (pais.toLowerCase() !== 'brasil' && pais.toLowerCase() !== 'br') {
-    console.log(`Vivendo em ${pais}`)
-    console.log('Você é Estrangeiro')
+if (pais.trim() === "") {
+  console.log("Você não forneceu um país válido.");
+} else if (pais.toLowerCase() !== "brasil" && pais.toLowerCase() !== "br") {
+  console.log(`Vivendo em ${pais}`);
+  console.log("Você é Estrangeiro");
 } else {
-    console.log(`Vivendo em ${pais}`)
-    console.log('Você é Brasileiro')
+  console.log(`Vivendo em ${pais}`);
+  console.log("Você é Brasileiro");
 }
